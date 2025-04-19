798d08f789916e23d8c4ddce33a6f4a2ffa263bd
Mritunjay Mishra <mritunjaymishra7607197286@gmail.com>
2025-04-19 21:55:31 +0530

    R

---
+++ 03tailwindprops/src/App.jsx
@@ -0,0 +1,75 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Card from './components/Card'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let myObj = {
+    username: "Mritunjay",
+    age:20
+  }
+
+  return (
+    <>
+      {/*
+       <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>    */}
+    {/* // <h1 className="text-3xl font-bold underline">
+    //   Hello world!
+    // </h1>
+   
+     */}
+    
+    
+      {/* <figure class="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
+  <img class="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://images.pexels.com/photos/15568759/pexels-photo-15568759/free-photo-of-bird-among-spring-blossoms-on-tree.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load" alt="" width="384" height="512" />
+  <div class="pt-6 md:p-8 text-center md:text-left space-y-4">
+    <blockquote>
+      <p class="text-lg font-medium">
+        “Tailwind CSS is the only framework that I've seen scale
+        on large teams. It’s easy to customize, adapts to any design,
+        and the build size is tiny.”
+      </p>
+    </blockquote>
+    <figcaption class="font-medium">
+      <div class="text-sky-500 dark:text-sky-400">
+        Sarah Dayan
+      </div>
+      <div class="text-slate-700 dark:text-slate-500">
+        Staff Engineer, Algolia
+      </div>
+    </figcaption>
+  </div>
+</figure> */}
+
+<h1 className='bg-green-400 text-black p-4 rounded-xl'>Mritunjay</h1>
+
+{/* <Card channel="chaiaurcode" someObje={myObj} /> */}
+<Card username="Chaiorcode" btnText="click me"/>
+<Card username="Mongo" btnText="visit me"/>
+
+    </>
+  )
+}
+
+export default App
